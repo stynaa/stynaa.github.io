bf5654decd8eaf4771f73b89750768e926ca7863
stynaa <styna.lo@gmail.com>
2019-08-01 12:37:22 -0600

    Reduce spacing between categories

--- js/custom.js
+++ js/custom.js
@@ -2,5 +2,18 @@ jQuery(document).ready(function() {
 	
 	"use strict";
 	// Your custom js code goes here.
+	
+	/*
+	$("#blog-cat").owlCarousel({
+		items:3,
+		loop:!0,
+		margin:20,
+		nav:!1,
+		dots:!1,
+		smartSpeed:800,
+		autoHeight:true,
+		navText:["<i class='icon-keyboard_arrow_left owl-direction'></i>","<i class='icon-keyboard_arrow_right owl-direction'></i>"],
+		responsive:{0:{items:1},400:{items:1},600:{items:2},1e3:{items:3}}
+	})*/
 
 });
